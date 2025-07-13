8079608fdfde1dbeb44dc935ba84ba39f2f83aa2
Talha Ashraf <tash.dev@proton.me>
2025-07-14 01:39:26 +1200

    feat: remove B from logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Talha Ashraf',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Talha Ashraf is an computer engineer who likes to solve problems',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Talha Ashraf',
+        short_name: 'Talha Ashraf',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
